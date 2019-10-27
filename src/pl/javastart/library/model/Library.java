eefdc9b1b6f74e51886c6e2628e91058a67c5138
Answer71 <mazurwro@gmail.com>
2019-10-27 23:17:25 +0100

    wersja 0.9 programu biblioteka
    wprowadzone pętle do dodawania i wyswietlania książek

---
+++ src/pl/javastart/library/model/Library.java
@@ -0,0 +1,27 @@
+package pl.javastart.library.model;
+
+public class Library {
+
+    private final int maxBooks = 100;
+    private Book[] books = new Book[maxBooks];
+    private int booksNumber = 0;
+    public void addBook(Book book) {
+        if (booksNumber < maxBooks) {
+            books[booksNumber] = book;
+            booksNumber++;
+        } else {
+            System.out.println("Maksymalna liczba książek zostałą osiągnieta");
+        }
+    }
+
+    public void printBooks() {
+        if (booksNumber == 0) {
+            System.out.println("Brak książek w bibliotece");
+        }
+        for (int i = 0; i < booksNumber; i++) {
+            books[i].printInfo();
+            
+        }
+    }
+
+}
