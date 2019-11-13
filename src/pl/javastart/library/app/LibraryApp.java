2af7585203d35d311ed84779a97ade8a04711b08
Answer71 <mazurwro@gmail.com>
2019-11-13 21:11:56 +0100

    wersja 1.0 programu biblioteka
    wprowadzone zmienne statyczne

--- src/pl/javastart/library/app/LibraryApp.java
+++ src/pl/javastart/library/app/LibraryApp.java
@@ -1,9 +1,10 @@
 package pl.javastart.library.app;
 
 public class LibraryApp {
+    private static final String APP_NAME = "Biblioteka v 1.0";
     public static void main(String[] args) {
         final String appName = "Biblioteka v 0.9";
-        System.out.println(appName);
+        System.out.println(APP_NAME);
         LibraryControl libControl = new LibraryControl();
         libControl.controlLoop();
     }
--- src/pl/javastart/library/app/LibraryControl.java
+++ src/pl/javastart/library/app/LibraryControl.java
@@ -6,9 +6,9 @@ import pl.javastart.library.model.Library;
 
 public class LibraryControl {
 
-    private final int exit = 0;
-    private final int addBook = 1;
-    private final int printBook = 2;
+    private static final int EXIT = 0;
+    private static final int ADD_BOOK = 1;
+    private static final int PRINT_BOOK = 2;
     private DataReader dataReader = new DataReader();
     private Library library = new Library();
 
@@ -19,19 +19,19 @@ public class LibraryControl {
             printOptions();
             option = dataReader.getInt();
             switch (option) {
-                case addBook:
+                case ADD_BOOK:
                     addBook();
                     break;
-                case printBook:
+                case PRINT_BOOK:
                     printBooks();
                     break;
-                case exit:
+                case EXIT:
                     exit();
                     break;
                 default:
                     System.out.println("Nie ma takiej opcji, wprowadz ponownie.");
             }
-        } while (option != exit);
+        } while (option != EXIT);
     }
 
     private void exit() {
@@ -51,8 +51,8 @@ public class LibraryControl {
 
     private void printOptions() {
         System.out.println("Wybierz opcję:");
-        System.out.println(exit + " - wyjcie z programu");
-        System.out.println(addBook + " - dodanie nowej książki");
-        System.out.println(printBook + " - wyświetl dostępne książki");
+        System.out.println(EXIT + " - wyjcie z programu");
+        System.out.println(ADD_BOOK + " - dodanie nowej książki");
+        System.out.println(PRINT_BOOK + " - wyświetl dostępne książki");
     }
 }
